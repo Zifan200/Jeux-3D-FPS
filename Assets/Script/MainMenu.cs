ddf598c6b6ac1f337642ed33c5afee04a6316060
Zifan Wang <lastrain95000@gmail.com>
2024-04-25 19:18:48 -0400

    navigation du menu titre vers menu principal implémentée

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    private GameObject menuTitre;
+    private GameObject menuPrincipal;
+    private GameObject menuJouer;
+    private GameObject menuQuitter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuPrincipal = GameObject.Find("MenuPrincipal");
+        menuTitre = GameObject.Find("MenuTitre");
+        menuJouer = GameObject.Find("MenuJouer");
+        menuQuitter = GameObject.Find("MenuQuitter"); 
+
+        menuPrincipal.SetActive(false); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void onButtonCommencer()
+    {
+        menuTitre.SetActive(false);
+        menuPrincipal.SetActive(true);
         
     }
 }
