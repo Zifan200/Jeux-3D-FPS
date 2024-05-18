c9327e1b41324d4f3dcca608574eacdd1d8608da
Zifan Wang <lastrain95000@gmail.com>
2024-05-17 23:06:15 -0400

    slider pour gerer le son et le volume implémenté

---
+++ Assets/Script/MenuOption.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuOption : MonoBehaviour
+{
+    [SerializeField] private Slider sliderSon;
+    [SerializeField] private Slider sliderMusique;
+    [SerializeField] private TextMeshProUGUI textSon;
+    [SerializeField] private TextMeshProUGUI textMusique;
+    [SerializeField] private AudioSource musicSource; // Reference au AudioSource de la musique
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set la valeur des sliders
+        sliderSon.onValueChanged.AddListener(delegate { onSliderVolume(); });
+        sliderMusique.onValueChanged.AddListener(delegate { onSliderMusique(); });
+
+        // Set le texte des sliders
+        textSon.text = (int)(sliderSon.value * 100) + "%";
+        textMusique.text = (int)(sliderMusique.value * 100) + "%";
+    }
+
+    public void onSliderMusique()
+    {
+        // Change the volume of the music and update the displayed volume
+        musicSource.volume = sliderMusique.value;
+        textMusique.text = (int)(sliderMusique.value * 100) + "%";
+    }
+
+    public void onSliderVolume()
+    {
+        // Update the displayed volume for sound effects
+        textSon.text = (int)(sliderSon.value * 100) + "%";
+    }
+}
